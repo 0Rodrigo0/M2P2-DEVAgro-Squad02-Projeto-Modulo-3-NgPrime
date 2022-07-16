02009894efc9e7037e17369e8aad9571f7c884bd
0Rodrigo0 <rodrigobl1@yahoo.com.br>
2022-07-16 12:12:39 -0300

    Task3

--- src/app/_shared/shared.module.ts
+++ src/app/_shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, ButtonModule],
+  imports: [CommonModule, ButtonModule, InputTextModule],
   exports: [],
 })
 export class SharedModule {}
